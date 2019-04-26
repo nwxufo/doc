619e111400df4e12a398092f1a587c0f10050276
X. Milo Niu <milo_niu@163.com>
2019-04-26 15:53:05 +0800

    commit at gxos-archlinux

---
+++ mindmap/Computer Science.mm
@@ -0,0 +1,137 @@
+<map version="freeplane 1.7.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Computer Science" FOLDED="false" ID="ID_729255827" CREATED="1556257222429" MODIFIED="1556262258425" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false" show_note_icons="true"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="5" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="Tools" LOCALIZED_STYLE_REF="defaultstyle.floating" POSITION="right" ID="ID_499069157" CREATED="1556262244017" MODIFIED="1556262263849" HGAP_QUANTITY="61.49999816715723 pt" VSHIFT_QUANTITY="-168.74999497085824 pt">
+<hook NAME="FreeNode"/>
+<node TEXT="ISO" ID="ID_726938536" CREATED="1556262268909" MODIFIED="1556262270734">
+<node TEXT="https://wiki.archlinux.org/index.php/Remastering_the_Install_ISO#Create_a_new_ISO" ID="ID_683227746" CREATED="1556262272301" MODIFIED="1556262275671"/>
+</node>
+</node>
+<node TEXT="Network" FOLDED="true" POSITION="right" ID="ID_1032620553" CREATED="1556257291181" MODIFIED="1556257300410">
+<edge COLOR="#ff0000"/>
+<node TEXT="Openvswitch" ID="ID_573876841" CREATED="1556257682485" MODIFIED="1556257689033"/>
+<node TEXT="Network Virtualization" ID="ID_1563764342" CREATED="1556257689493" MODIFIED="1556257720361"/>
+<node TEXT="TCP/IP" ID="ID_1944157074" CREATED="1556257720813" MODIFIED="1556257743457"/>
+</node>
+<node TEXT="Operating System" POSITION="right" ID="ID_391987504" CREATED="1556257386661" MODIFIED="1556257397309">
+<edge COLOR="#ff00ff"/>
+<node TEXT="FreeBSD" ID="ID_577510616" CREATED="1556257583509" MODIFIED="1556257587585"/>
+<node TEXT="Linux" FOLDED="true" ID="ID_1353816174" CREATED="1556257587949" MODIFIED="1556257590057">
+<node TEXT="GRUB2" ID="ID_92507077" CREATED="1556257619565" MODIFIED="1556257623497"/>
+<node TEXT="kernel" ID="ID_1570794311" CREATED="1556257624205" MODIFIED="1556257629937">
+<node TEXT="Rand the address of kernel image" ID="ID_232768581" CREATED="1556257982525" MODIFIED="1556258008257"/>
+<node TEXT="Randomize the memory sections" ID="ID_750240933" CREATED="1556258008941" MODIFIED="1556258033697"/>
+</node>
+<node TEXT="main//program entry point" ID="ID_435635668" CREATED="1556257636061" MODIFIED="1556257658784"/>
+</node>
+<node TEXT="Security" ID="ID_794161996" CREATED="1556262035310" MODIFIED="1556262038537">
+<node TEXT="https://security.archlinux.org/" ID="ID_9615299" CREATED="1556262045677" MODIFIED="1556262047929"/>
+</node>
+</node>
+<node TEXT="compiler" POSITION="right" ID="ID_1304598372" CREATED="1556257301885" MODIFIED="1556257309834">
+<edge COLOR="#0000ff"/>
+<node TEXT="GDB" ID="ID_1101127434" CREATED="1556257547037" MODIFIED="1556257549609">
+<node TEXT="Buffer Overflow" ID="ID_1939897854" CREATED="1556258189517" MODIFIED="1556260705370"><richcontent TYPE="NOTE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      cn.bing.com:
+    </p>
+    <p>
+      &#160;&#160;&#160;&#160;&#160;&#160;key words: shell_code buffer overflow
+    </p>
+  </body>
+</html>
+
+</richcontent>
+<node TEXT="http://www.cs.umd.edu/class/fall2012/cmsc498L/materials/vuln-lab.shtml" ID="ID_286787273" CREATED="1556260309373" MODIFIED="1556260346531"/>
+<node TEXT="https://dhavalkapil.com/blogs/Shellcode-Injection/" ID="ID_334328333" CREATED="1556260701381" MODIFIED="1556260708291"/>
+</node>
+</node>
+<node TEXT="GCC" ID="ID_1101628151" CREATED="1556257349357" MODIFIED="1556257353762"/>
+<node TEXT="LLVM" ID="ID_157089015" CREATED="1556257354189" MODIFIED="1556257357265"/>
+<node TEXT="QUME" ID="ID_1948698397" CREATED="1556257365045" MODIFIED="1556257369041"/>
+</node>
+<node TEXT="Architecture" POSITION="right" ID="ID_269836465" CREATED="1556257310733" MODIFIED="1556257533469">
+<edge COLOR="#00ff00"/>
+<node TEXT="x86" ID="ID_1715133479" CREATED="1556257326613" MODIFIED="1556257533468">
+<node TEXT="assembly" ID="ID_970889662" CREATED="1556258294101" MODIFIED="1556258299370"/>
+</node>
+<node TEXT="RISC-V" ID="ID_1843939793" CREATED="1556257331733" MODIFIED="1556261339795">
+<node TEXT="Computer Organization and Design: RISV-V Edition" ID="ID_275046070" CREATED="1556257429669" MODIFIED="1556257464370"/>
+<node TEXT="The RISC-V READER" ID="ID_1718157043" CREATED="1556257465141" MODIFIED="1556261339794"/>
+<node TEXT="Computer Architecture: A Quantitative  Approach" ID="ID_126786531" CREATED="1556257477291" MODIFIED="1556257530154"/>
+</node>
+</node>
+<node TEXT="Data Analysis" POSITION="right" ID="ID_249163168" CREATED="1556258371165" MODIFIED="1556258376913">
+<edge COLOR="#00ffff"/>
+</node>
+</node>
+</map>
---
+++ mindmap/MyWebsit.mm
@@ -0,0 +1,110 @@
+<map version="freeplane 1.7.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="MyWebsit" FOLDED="false" ID="ID_1016524144" CREATED="1556015287699" MODIFIED="1556015296926" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false" show_note_icons="true"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="3" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="Make it work for you Document" POSITION="right" ID="ID_1037017327" CREATED="1556015300943" MODIFIED="1556015317043">
+<edge COLOR="#ff0000"/>
+<node TEXT="pelican" ID="ID_1406594310" CREATED="1556015318583" MODIFIED="1556015323515"/>
+<node TEXT="jupter" ID="ID_828440716" CREATED="1556015323863" MODIFIED="1556015331916"/>
+<node TEXT="elegant" ID="ID_463762644" CREATED="1556015332471" MODIFIED="1556015337420"/>
+</node>
+<node TEXT="" POSITION="left" ID="ID_1838564308" CREATED="1556015349759" MODIFIED="1556015349761">
+<edge COLOR="#0000ff"/>
+</node>
+<node TEXT="make your websit work" POSITION="right" ID="ID_1180714112" CREATED="1556015351918" MODIFIED="1556016281748">
+<edge COLOR="#00ff00"/>
+<hook URI="MyWebsit_files/architecture.png" SIZE="0.75" NAME="ExternalObject"/>
+<richcontent TYPE="NOTE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <span http-equiv="content-type" content="text/html; charset=utf-8" style="color: rgb(0, 0, 0); font-family: PingFangSC, helvetica neue, Helvetica, Arial, sans-serif; font-size: 14px; font-style: normal; font-weight: 400; letter-spacing: normal; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; word-spacing: 0px; background-color: rgb(255, 255, 255); display: inline !important; float: none"><font color="rgb(0, 0, 0)" face="PingFangSC, helvetica neue, Helvetica, Arial, sans-serif" size="14px">Web App&#65292;Datastore&#65292;Async Worker&#65292;Task Queue&#65292;&#20197;&#21450; Scheduler&#12290;&#20854;&#20013;&#65292;Web App&#23601;&#26159;&#36305;&#32593;&#31449;&#20195;&#30721;&#25509;&#21463;&#29992;&#25143;&#30340;&#35775;&#38382;&#35831;&#27714;&#65292;&#25152;&#26377;&#32791;&#26102;&#38388;&#30340; task&#65288;&#27604;&#22914;&#21457;&#37038;&#20214;&#65292;&#21457;&#24494;&#21338;&#65292;&#25968;&#25454;&#32479;&#35745;&#31561;&#65289;&#37117;&#25172;&#21040; Task Queue&#19978;&#65292;&#28982;&#21518; Async Worker &#20174; Task Queue &#25235; task &#36807;&#26469;&#31163;&#32447;&#22788;&#29702;&#65307;&#32780; Scheduler &#23601;&#26159;&#23450;&#26102;&#36305;&#31243;&#24207;&#65292;&#24456;&#22810;&#32593;&#31449;&#30452;&#25509;&#29992;&#30340; Cron</font></span>
+    </p>
+  </body>
+</html>
+
+</richcontent>
+<node TEXT="An implementation" ID="ID_1406216878" CREATED="1556016301007" MODIFIED="1556016318194"><richcontent TYPE="NOTE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <span http-equiv="content-type" content="text/html; charset=utf-8" style="color: rgb(0, 0, 0); font-family: PingFangSC, helvetica neue, Helvetica, Arial, sans-serif; font-size: 14px; font-style: normal; font-weight: 400; letter-spacing: normal; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; word-spacing: 0px; background-color: rgb(255, 255, 255); display: inline !important; float: none"><font color="rgb(0, 0, 0)" face="PingFangSC, helvetica neue, Helvetica, Arial, sans-serif" size="14px">Web App &#23601;&#26159;&#160;</font></span><a class="markup--anchor markup--p-anchor" href="https://www.djangoproject.com/" rel="nofollow" data-href="https://www.djangoproject.com/" style="background-color: rgb(255, 255, 255); color: rgb(221, 72, 20); text-decoration: none; font-weight: 500; font-family: PingFangSC, helvetica neue, Helvetica, Arial, sans-serif; font-size: 14px; font-style: normal; letter-spacing: normal; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; word-spacing: 0px"><font color="rgb(221, 72, 20)" face="PingFangSC, helvetica neue, Helvetica, Arial, sans-serif" size="14px"><b>Django</b></font></a><span><font color="rgb(0, 0, 0)" face="PingFangSC, helvetica neue, Helvetica, Arial, sans-serif" size="14px">&#160;</font></span><font color="rgb(0, 0, 0)" face="PingFangSC, helvetica neue, Helvetica, Arial, sans-serif" size="14px"><span style="color: rgb(0, 0, 0); font-family: PingFangSC, helvetica neue, Helvetica, Arial, sans-serif; font-size: 14px; font-style: normal; font-weight: 400; letter-spacing: normal; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; word-spacing: 0px; background-color: rgb(255, 255, 255); display: inline !important; float: none">App&#65292;&#29992;&#160;</span></font><a class="markup--anchor markup--p-anchor" href="https://uwsgi-docs.readthedocs.org/en/latest/" rel="nofollow" data-href="https://uwsgi-docs.readthedocs.org/en/latest/" style="background-color: rgb(255, 255, 255); color: rgb(221, 72, 20); text-decoration: none; font-weight: 500; font-family: PingFangSC, helvetica neue, Helvetica, Arial, sans-serif; font-size: 14px; font-style: normal; letter-spacing: normal; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; word-spacing: 0px"><font color="rgb(221, 72, 20)" face="PingFangSC, helvetica neue, Helvetica, Arial, sans-serif" size="14px"><b>uwsgi</b></font></a><span><font color="rgb(0, 0, 0)" face="PingFangSC, helvetica neue, Helvetica, Arial, sans-serif" size="14px">&#160;</font></span><font color="rgb(0, 0, 0)" face="PingFangSC, helvetica neue, Helvetica, Arial, sans-serif" size="14px"><span style="color: rgb(0, 0, 0); font-family: PingFangSC, helvetica neue, Helvetica, Arial, sans-serif; font-size: 14px; font-style: normal; font-weight: 400; letter-spacing: normal; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; word-spacing: 0px; background-color: rgb(255, 255, 255); display: inline !important; float: none">&#36305;N&#20010;&#36827;&#31243;&#65292;&#29992;</span><span>&#160;</span></font><a class="markup--anchor markup--p-anchor" href="http://supervisord.org/" rel="nofollow" data-href="http://supervisord.org/" style="background-color: rgb(255, 255, 255); color: rgb(221, 72, 20); text-decoration: none; font-weight: 500; font-family: PingFangSC, helvetica neue, Helvetica, Arial, sans-serif; font-size: 14px; font-style: normal; letter-spacing: normal; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; word-spacing: 0px"><font color="rgb(221, 72, 20)" face="PingFangSC, helvetica neue, Helvetica, Arial, sans-serif" size="14px"><b>supervisord</b></font></a><span><font color="rgb(0, 0, 0)" face="PingFangSC, helvetica neue, Helvetica, Arial, sans-serif" size="14px">&#160;</font></span><font color="rgb(0, 0, 0)" face="PingFangSC, helvetica neue, Helvetica, Arial, sans-serif" size="14px"><span style="color: rgb(0, 0, 0); font-family: PingFangSC, helvetica neue, Helvetica, Arial, sans-serif; font-size: 14px; font-style: normal; font-weight: 400; letter-spacing: normal; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; word-spacing: 0px; background-color: rgb(255, 255, 255); display: inline !important; float: none">&#26469;&#31649;&#29702;&#36827;&#31243;&#65292;&#21069;&#38754;&#25346;&#19968;&#20010;</span><span>&#160;</span></font><a class="markup--anchor markup--p-anchor" href="https://www.nginx.com/" rel="nofollow" data-href="https://www.nginx.com/" style="background-color: rgb(255, 255, 255); color: rgb(221, 72, 20); text-decoration: none; font-weight: 500; font-family: PingFangSC, helvetica neue, Helvetica, Arial, sans-serif; font-size: 14px; font-style: normal; letter-spacing: normal; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; word-spacing: 0px"><font color="rgb(221, 72, 20)" face="PingFangSC, helvetica neue, Helvetica, Arial, sans-serif" size="14px"><b>nginx</b></font></a><span><font color="rgb(0, 0, 0)" face="PingFangSC, helvetica neue, Helvetica, Arial, sans-serif" size="14px">&#160;</font></span><font color="rgb(0, 0, 0)" face="PingFangSC, helvetica neue, Helvetica, Arial, sans-serif" size="14px"><span style="color: rgb(0, 0, 0); font-family: PingFangSC, helvetica neue, Helvetica, Arial, sans-serif; font-size: 14px; font-style: normal; font-weight: 400; letter-spacing: normal; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; word-spacing: 0px; background-color: rgb(255, 255, 255); display: inline !important; float: none">&#24403; load balancer&#12290;Database&#29992;&#30340;&#26159;&#160;</span></font><a class="markup--anchor markup--p-anchor" href="http://www.postgresql.org/" rel="nofollow" data-href="http://www.postgresql.org/" style="background-color: rgb(255, 255, 255); color: rgb(221, 72, 20); text-decoration: none; font-weight: 500; font-family: PingFangSC, helvetica neue, Helvetica, Arial, sans-serif; font-size: 14px; font-style: normal; letter-spacing: normal; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; word-spacing: 0px"><font color="rgb(221, 72, 20)" face="PingFangSC, helvetica neue, Helvetica, Arial, sans-serif" size="14px"><b>Postgres</b></font></a><span><font color="rgb(0, 0, 0)" face="PingFangSC, helvetica neue, Helvetica, Arial, sans-serif" size="14px">&#160;</font></span><font color="rgb(0, 0, 0)" face="PingFangSC, helvetica neue, Helvetica, Arial, sans-serif" size="14px"><span style="color: rgb(0, 0, 0); font-family: PingFangSC, helvetica neue, Helvetica, Arial, sans-serif; font-size: 14px; font-style: normal; font-weight: 400; letter-spacing: normal; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; word-spacing: 0px; background-color: rgb(255, 255, 255); display: inline !important; float: none">&#19982;</span><span>&#160;</span></font><a class="markup--anchor markup--p-anchor" href="http://redis.io/" data-href="http://redis.io/" style="background-color: rgb(255, 255, 255); color: rgb(221, 72, 20); text-decoration: none; font-weight: 500; font-family: PingFangSC, helvetica neue, Helvetica, Arial, sans-serif; font-size: 14px; font-style: normal; letter-spacing: normal; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; word-spacing: 0px"><font color="rgb(221, 72, 20)" face="PingFangSC, helvetica neue, Helvetica, Arial, sans-serif" size="14px"><b>Redis</b></font></a><span style="color: rgb(0, 0, 0); font-family: PingFangSC, helvetica neue, Helvetica, Arial, sans-serif; font-size: 14px; font-style: normal; font-weight: 400; letter-spacing: normal; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; word-spacing: 0px; background-color: rgb(255, 255, 255); display: inline !important; float: none"><font color="rgb(0, 0, 0)" face="PingFangSC, helvetica neue, Helvetica, Arial, sans-serif" size="14px">&#160;&#8202; &#160;&#8212;&#8202; &#20854;&#20013;&#65292;&#22823;&#37096;&#20998;&#38656;&#35201;&#27704;&#20037;&#23384;&#20648;&#30340;&#25968;&#25454;&#37117;&#22312;Postgres&#20013;&#65292;&#32780;Redis&#23384;&#30340;&#26159;&#25991;&#31456;&#30340;&#35775;&#38382;&#25968;&#19982;&#19968;&#20123;&#21482;&#38656;&#20445;&#30041;&#19968;&#20004;&#22825;&#30340;&#25968;&#25454;&#12290;Task Queue&#26159;&#29992;</font></span><font color="rgb(0, 0, 0)" face="PingFangSC, helvetica neue, Helvetica, Arial, sans-serif" size="14px"><span>&#160;</span></font><a class="markup--anchor markup--p-anchor" href="http://www.rabbitmq.com/" rel="nofollow" data-href="http://www.rabbitmq.com/" style="background-color: rgb(255, 255, 255); color: rgb(221, 72, 20); text-decoration: none; font-weight: 500; font-family: PingFangSC, helvetica neue, Helvetica, Arial, sans-serif; font-size: 14px; font-style: normal; letter-spacing: normal; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; word-spacing: 0px"><font color="rgb(221, 72, 20)" face="PingFangSC, helvetica neue, Helvetica, Arial, sans-serif" size="14px"><b>RabbitMQ</b></font></a><span style="color: rgb(0, 0, 0); font-family: PingFangSC, helvetica neue, Helvetica, Arial, sans-serif; font-size: 14px; font-style: normal; font-weight: 400; letter-spacing: normal; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; word-spacing: 0px; background-color: rgb(255, 255, 255); display: inline !important; float: none"><font color="rgb(0, 0, 0)" face="PingFangSC, helvetica neue, Helvetica, Arial, sans-serif" size="14px">&#12290;Scheduler&#29992;&#30340;&#26159;</font></span><font color="rgb(0, 0, 0)" face="PingFangSC, helvetica neue, Helvetica, Arial, sans-serif" size="14px"><span>&#160;</span></font><a class="markup--anchor markup--p-anchor" href="http://docs.celeryproject.org/en/latest/userguide/periodic-tasks.html" rel="nofollow" data-href="http://docs.celeryproject.org/en/latest/userguide/periodic-tasks.html" style="background-color: rgb(255, 255, 255); color: rgb(221, 72, 20); text-decoration: none; font-weight: 500; font-family: PingFangSC, helvetica neue, Helvetica, Arial, sans-serif; font-size: 14px; font-style: normal; letter-spacing: normal; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; word-spacing: 0px"><font color="rgb(221, 72, 20)" face="PingFangSC, helvetica neue, Helvetica, Arial, sans-serif" size="14px"><b>Celery Beat</b></font></a><span style="color: rgb(0, 0, 0); font-family: PingFangSC, helvetica neue, Helvetica, Arial, sans-serif; font-size: 14px; font-style: normal; font-weight: 400; letter-spacing: normal; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; word-spacing: 0px; background-color: rgb(255, 255, 255); display: inline !important; float: none"><font color="rgb(0, 0, 0)" face="PingFangSC, helvetica neue, Helvetica, Arial, sans-serif" size="14px">&#12290;&#32780;Async Worker&#26159;</font></span><font color="rgb(0, 0, 0)" face="PingFangSC, helvetica neue, Helvetica, Arial, sans-serif" size="14px"><span>&#160;</span></font><a class="markup--anchor markup--p-anchor" href="http://docs.celeryproject.org/en/latest/index.html" rel="nofollow" data-href="http://docs.celeryproject.org/en/latest/index.html" style="background-color: rgb(255, 255, 255); color: rgb(221, 72, 20); text-decoration: none; font-weight: 500; font-family: PingFangSC, helvetica neue, Helvetica, Arial, sans-serif; font-size: 14px; font-style: normal; letter-spacing: normal; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; word-spacing: 0px"><font color="rgb(221, 72, 20)" face="PingFangSC, helvetica neue, Helvetica, Arial, sans-serif" size="14px"><b>Celery</b></font></a><span style="color: rgb(0, 0, 0); font-family: PingFangSC, helvetica neue, Helvetica, Arial, sans-serif; font-size: 14px; font-style: normal; font-weight: 400; letter-spacing: normal; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; word-spacing: 0px; background-color: rgb(255, 255, 255); display: inline !important; float: none"><font color="rgb(0, 0, 0)" face="PingFangSC, helvetica neue, Helvetica, Arial, sans-serif" size="14px">&#12290;</font></span>
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+</node>
+</node>
+</map>
