cbf10231a6358036fa21e89315aaacb0b284bb4c
X. Milo Niu <milo_niu@163.com>
2019-04-27 22:09:39 +0800

    add doc 20190427

---
+++ arch_install/arch_install_pkgins.cmd
@@ -0,0 +1,5 @@
+pacstrap /mnt git openssh mutt slim esmtp wireless_tools wpa_supplicant wpa_actiond dialog icewm rxvt-unicode xterm  lxc vim wget lftp xorg-init xorg-server xorg-server-utils fcitx alsa-utils flashplugin
+pacstrap /mnt firefox chromium smplayer
+pacstrap /mnt fcitx flashplugin alsa-utils
+pacstrap /mnt  xorg-server xorg-server-utils 
+pacstrap /mnt gummi gimp vinagre geany viewnior
